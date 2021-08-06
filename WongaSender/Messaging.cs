c9dec3773fbe1146e6c1d602219d4258f07b3e36
developerriaz <59824173+developerriaz@users.noreply.github.com>
2021-08-06 12:00:13 +0200

    First Push.
    First push to the git repo.

---
+++ WongaSender/Messaging.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace WongaSender
+{
+    /// <summary>Class <c>Messaging</c> for sending a messaging in JSON-format to the Wonga Application
+    /// </summary>
+    ///
+    class Messaging
+    {
+        private String name;
+        private String queueName = "WONGA_QUEUE";
+        public Messaging(String senderName)
+        {
+            this.name = senderName;
+        }
+
+        public bool Initiate()
+        {
+            if (this.name == null)
+            {
+                Console.WriteLine("You needed to place your name to send a message\nOooops!!!");
+                return false;
+            }
+            ConnectionFactory factory = new ConnectionFactory();
+
+            factory.Uri = new System.Uri("amqp://guest:guest@localhost:8000");
+
+            IConnection connection = factory.CreateConnection();
+            var f = connection.CreateModel();
+            f.QueueDeclare(this.queueName, durable: true, exclusive: false, autoDelete: true, arguments: null);
+
+            byte[] dataToSend = Encoding.UTF8.GetBytes(this.name);
+            String outputMessage = "Hello, my name is " + this.name;
+            //Display message before Sending name of the user
+            Console.WriteLine(outputMessage);
+            Console.WriteLine("We will make sure your name is sent.\nThank you for using our services.");
+
+            //Send the data to the Platform
+            f.BasicPublish("", this.queueName, null, body: dataToSend);
+
+            f.Abort();
+
+            return true;
+        }
+    }
+}
---
+++ WongaSender/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WongaSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to Wonga Messaging System.\n\n"
+                + "Please enter your name:");
+
+            String name = Console.ReadLine();
+
+            if ((new Messaging(name).Initiate()) == true)
+            {
+                Console.WriteLine("Your name has been sent to the Wonga Application"
+                    + "\nPlease check for any changes on the Wonga Appliction"
+                    + "\nThank you so much.");
+            }
+            else
+            {
+                Console.WriteLine("For some reason the we failed to forward your name to Wonga Applicaiton");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Wonga Technologies Copyright 2021 - Powered by Riaz Hlatshwayo");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Read();
+        }
+    }
+}
